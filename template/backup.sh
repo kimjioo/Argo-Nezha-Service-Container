cf9002604849828eeac727f4c1c7b9436eaa424c
kimjioo <82633446+kimjioo@users.noreply.github.com>
2024-07-08 11:13:55 +0800

    Update backup.sh

--- template/backup.sh
+++ template/backup.sh
@@ -3,7 +3,7 @@
 # backup.sh 传参 a 自动还原； 传参 m 手动还原； 传参 f 强制更新面板 app 文件及 cloudflared 文件，并备份数据至成备份库。
 # 如是 IPv6 only 或者大陆机器，需要 Github 加速网，可自行查找放在 GH_PROXY 处 ，如 https://mirror.ghproxy.com/ ，能不用就不用，减少因加速网导致的故障。
 
-GH_PROXY=
+#GH_PROXY=
 GH_PAT=
 GH_BACKUP_USER=
 GH_EMAIL=

c3bfb303d73c068720177bab1b5a35d84cd52b26
kimjioo <82633446+kimjioo@users.noreply.github.com>
2024-07-08 11:12:05 +0800

    Update dashboard.sh

--- dashboard.sh
+++ dashboard.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 各变量默认值
-GH_PROXY='https://ghproxy.lvedong.eu.org/'
+#GH_PROXY='https://ghproxy.lvedong.eu.org/'
 WORK_DIR='/opt/nezha/dashboard'
 TEMP_DIR='/tmp/nezha'
 START_PORT='5000'
